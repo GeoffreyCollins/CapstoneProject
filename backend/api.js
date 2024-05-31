8bf8c4c3cce84e65ff5b6837d373f0a3a5735743
GeoffreyCollins <collinsgeo@byui.edu>
2024-05-31 12:09:15 -0600

    added compression middleware

--- backend/api.js
+++ backend/api.js
@@ -7,6 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Middleware
+app.use(compression());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev')); // Add logging
