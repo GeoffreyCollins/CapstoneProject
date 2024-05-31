174965bd879945e6b820cad948f6eb5dac6733e5
GeoffreyCollins <collinsgeo@byui.edu>
2024-05-31 12:09:54 -0600

    added compression

--- backend/api.js
+++ backend/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
+const compression = require('compression');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
