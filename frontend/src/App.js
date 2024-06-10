a0b2693fe88b218b60eef0b27058cbd8099a06f6
GeoffreyCollins <collinsgeo@byui.edu>
2024-06-10 14:10:25 -0600

    made some changes but they did not compile well at all. I will revert back to the last working version

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,39 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import FAQPage from './pages/FAQPage';
-import Navbar from './components/Navbar';
+import React, { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import axios from 'axios';
+import 'leaflet/dist/leaflet.css';
 
 const App = () => {
+  const [city, setCity] = useState('');
+  const [location, setLocation] = useState(null);
+
+  const handleInputChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  const handleSearch = async () => {
+    const response = await axios.get(`http://localhost:5001/city/${city}`);
+    setLocation(response.data.location);
+  };
+
   return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/faq" component={FAQPage} />
-      </Switch>
-    </Router>
+    <div>
+      <h1>Carbon Tracker</h1>
+      <input type="text" value={city} onChange={handleInputChange} placeholder="Enter city name" />
+      <button onClick={handleSearch}>Search</button>
+      {location && (
+        <MapContainer center={[location.lat, location.lon]} zoom={13} style={{ height: '400px', width: '100%' }}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[location.lat, location.lon]}>
+            <Popup>
+              {city}
+            </Popup>
+          </Marker>
+        </MapContainer>
+      )}
+    </div>
   );
 };
 
